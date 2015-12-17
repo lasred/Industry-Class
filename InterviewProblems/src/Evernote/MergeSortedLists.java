023e9d4612882acba60fa95765fd3f48d3515d69
Chris Sun <chris438@gmail.com>
2015-12-17 10:41:41 -0800

    Merge sorted lists handles case where both lists are empty

--- InterviewProblems/src/Evernote/MergeSortedLists.java
+++ InterviewProblems/src/Evernote/MergeSortedLists.java
@@ -18,93 +18,95 @@ public class MergeSortedLists {
     public static <T extends Comparable<T>> List<T> mergeSortedList(
     		List<T> sortedList1, List<T> sortedList2) {
         List<T> resultList = new ArrayList<T>();
-        Iterator<T> iterator1 = sortedList1.iterator();
-        Iterator<T> iterator2 = sortedList2.iterator();
-        T value1 = iterator1.next();
-        while(value1 == null) {
-        	value1 = iterator1.next();
-        }
-        T value2 = iterator2.next();
-        while(value2 == null) {
-        	value2 = iterator2.next();
-        }
-        while(iterator1.hasNext() && iterator2.hasNext()) {
-              if(value1.compareTo(value2) <= 0) {
-                  resultList.add(value1);
-                  //move iterator cursor 
-                  if(iterator1.hasNext()) {
-                    value1 = iterator1.next();
-                  }
-              } else {
-                  resultList.add(value2);
-                  if(iterator2.hasNext()) {
-                    value2 = iterator2.next();
-                  }
-              }
-        }
-        //know that both value1 and value2's current value have not been added to the result list bc these things happen in the while loop
-        if(iterator1.hasNext() || iterator2.hasNext()) {
-            //know there is one thing in one list, find out where it fits in the remaining list 
-            if(iterator1.hasNext()) {
-                    //theres at least two elements in the first list that have not been added. Only one element(value2) needs to be added from list 2
-                    if(value2.compareTo(value1) <= 0) {
-                        resultList.add(value2);
-                        resultList.add(value1);
-                        while(iterator1.hasNext()) {
-                            resultList.add(iterator1.next());
-                        }
-                    } else {
-                        resultList.add(value1);
-                        boolean hasAddedValue2 = false;
-                        while(iterator1.hasNext()) {
-                            T nextValue = iterator1.next();
-                            if(value2.compareTo(nextValue) <= 0) {
-                                resultList.add(value2);
-                                hasAddedValue2 = true;
-                            }
-                            //no matter what, this needs to execute 
-                            resultList.add(nextValue);
-                        }
-                        //if not added, add it now(greatest element)
-                        if(!hasAddedValue2) {
-                        	resultList.add(value2);
-                        }
-                    }
-            } else {
-                if(value1.compareTo(value2) <= 0) {
-                    //add it in front
-                    resultList.add(value1);
-                    resultList.add(value2);
-                    while(iterator2.hasNext()) {
-                        resultList.add(iterator2.next());
-                    }
-                }
-                while(iterator2.hasNext()) {
-                    //add it in the middle
-                    resultList.add(value2);
-                    boolean hasAddedValue1 = false;
-                    while(iterator2.hasNext()) {
-                        T nextValue = iterator2.next();
-                        if(value1.compareTo(nextValue) <= 0) {
-                            resultList.add(value1);
-                            hasAddedValue1 = true;
-                        }
-                        resultList.add(nextValue);
-                    }
-                    if(!hasAddedValue1) {
-                    	resultList.add(value1);
-                    }
-                }
-            }
-        } else {
-            //need to add both
-            if(value1.compareTo(value2) <= 0) {
-                resultList.add(value1);
-                resultList.add(value2);
-            } else {
-                resultList.add(value2);
-                resultList.add(value1);
-            }
+        if(sortedList1.size() > 0 && sortedList2.size() > 0) {
+	        Iterator<T> iterator1 = sortedList1.iterator();
+	        Iterator<T> iterator2 = sortedList2.iterator();
+	        T value1 = iterator1.next();
+	        while(value1 == null) {
+	        	value1 = iterator1.next();
+	        }
+	        T value2 = iterator2.next();
+	        while(value2 == null) {
+	        	value2 = iterator2.next();
+	        }
+	        while(iterator1.hasNext() && iterator2.hasNext()) {
+	              if(value1.compareTo(value2) <= 0) {
+	                  resultList.add(value1);
+	                  //move iterator cursor 
+	                  if(iterator1.hasNext()) {
+	                    value1 = iterator1.next();
+	                  }
+	              } else {
+	                  resultList.add(value2);
+	                  if(iterator2.hasNext()) {
+	                    value2 = iterator2.next();
+	                  }
+	              }
+	        }
+	        //know that both value1 and value2's current value have not been added to the result list bc these things happen in the while loop
+	        if(iterator1.hasNext() || iterator2.hasNext()) {
+	            //know there is one thing in one list, find out where it fits in the remaining list 
+	            if(iterator1.hasNext()) {
+	                    //theres at least two elements in the first list that have not been added. Only one element(value2) needs to be added from list 2
+	                    if(value2.compareTo(value1) <= 0) {
+	                        resultList.add(value2);
+	                        resultList.add(value1);
+	                        while(iterator1.hasNext()) {
+	                            resultList.add(iterator1.next());
+	                        }
+	                    } else {
+	                        resultList.add(value1);
+	                        boolean hasAddedValue2 = false;
+	                        while(iterator1.hasNext()) {
+	                            T nextValue = iterator1.next();
+	                            if(value2.compareTo(nextValue) <= 0) {
+	                                resultList.add(value2);
+	                                hasAddedValue2 = true;
+	                            }
+	                            //no matter what, this needs to execute 
+	                            resultList.add(nextValue);
+	                        }
+	                        //if not added, add it now(greatest element)
+	                        if(!hasAddedValue2) {
+	                        	resultList.add(value2);
+	                        }
+	                    }
+	            } else {
+	                if(value1.compareTo(value2) <= 0) {
+	                    //add it in front
+	                    resultList.add(value1);
+	                    resultList.add(value2);
+	                    while(iterator2.hasNext()) {
+	                        resultList.add(iterator2.next());
+	                    }
+	                }
+	                while(iterator2.hasNext()) {
+	                    //add it in the middle
+	                    resultList.add(value2);
+	                    boolean hasAddedValue1 = false;
+	                    while(iterator2.hasNext()) {
+	                        T nextValue = iterator2.next();
+	                        if(value1.compareTo(nextValue) <= 0) {
+	                            resultList.add(value1);
+	                            hasAddedValue1 = true;
+	                        }
+	                        resultList.add(nextValue);
+	                    }
+	                    if(!hasAddedValue1) {
+	                    	resultList.add(value1);
+	                    }
+	                }
+	            }
+	        } else {
+	            //need to add both
+	            if(value1.compareTo(value2) <= 0) {
+	                resultList.add(value1);
+	                resultList.add(value2);
+	            } else {
+	                resultList.add(value2);
+	                resultList.add(value1);
+	            }
+	        }
         }
     	return resultList;	    	
     }
